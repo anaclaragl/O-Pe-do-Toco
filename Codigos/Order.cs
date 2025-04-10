10e5f9437826be2fcac99290bdca943bd9d83cb0
Ana Clara Linhares <75282985+anaclaragl@users.noreply.github.com>
2025-04-10 11:46:49 -0300

    Enum maiusculo

--- Codigos/Order.cs
+++ Codigos/Order.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Order")]
 public class Order : ScriptableObject
 {
-    private enum dietType{ Carnivore, Herbivore };
+    private enum dietType{ CARNIVORE, HERBIVORE };
 
     [Header("Item Description")]
     public string plateName;
