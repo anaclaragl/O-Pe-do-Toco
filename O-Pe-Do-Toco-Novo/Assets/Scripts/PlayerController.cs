75b34a43690c4e17cce29fff8fbc36b47085a79c
anaclaragl <ag.linhares2@gmail.com>
2025-04-13 20:28:14 -0300

    Walk Idle Pinguim (Player1)

--- O-Pe-Do-Toco-Novo/Assets/Scripts/PlayerController.cs
+++ O-Pe-Do-Toco-Novo/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    public PlayerLocomotion PlayerLocomotion;
+    public PlayerLocomotion playerLocomotion;
 
+    public void Start(){
+        animator = GetComponentInChildren<Animator>();
+        playerLocomotion = GetComponent<PlayerLocomotion>();
+    }
+
+    public void Update(){
+        animator.SetFloat("Magnitude", playerLocomotion.movement.magnitude);
+    }
 }
